054e9a493fb4f3da869abe7652a3cab1c27a7ff5
Simeon Widdis <sawiddis@gmail.com>
2026-05-16 17:47:55 -0700

    make game logging depend on arg

--- src/main.zig
+++ src/main.zig
@@ -1,32 +1,69 @@
 const std = @import("std");
 const Io = std.Io;
 
+const IO_BUF_SIZE = 2048;
+
 const Engine = @import("engine.zig").Engine;
+
+const CommandLog = struct {
+    active: bool = false,
+    dest_file: []u8 = undefined,
+    buffer: []u8 = undefined,
+    file: std.Io.File = undefined,
+    writer: std.Io.File.Writer = undefined,
+
+    fn init(alloc: std.mem.Allocator, io: Io, log_dir: [*:0]const u8) !CommandLog {
+        var self: CommandLog = .{};
+        self.buffer = try alloc.alloc(u8, IO_BUF_SIZE);
+        errdefer alloc.free(self.buffer);
+
+        var dir = Io.Dir.cwd();
+        const start_time = Io.Timestamp.now(io, Io.Clock.awake);
+        self.dest_file = try std.fmt.allocPrint(alloc, "{s}/log_{d}.log", .{ log_dir, start_time });
+        errdefer alloc.free(self.dest_file);
+
+        self.file = try dir.createFile(io, self.dest_file, .{});
+        self.writer = self.file.writer(io, self.buffer);
+        self.active = true;
+
+        return self;
+    }
+
+    fn deinit(self: *CommandLog, alloc: std.mem.Allocator, io: Io) void {
+        if (!self.active) return;
+
+        self.writer.flush() catch {};
+
+        self.file.close(io);
+        alloc.free(self.dest_file);
+        alloc.free(self.buffer);
+    }
+
+    fn write(self: *CommandLog, log_line: []const u8) !void {
+        if (!self.active) return;
+        try self.writer.interface.print("{s}\n", .{log_line});
+    }
+};
 
 pub fn main(init: std.process.Init) !void {
     var engine = try Engine.init(init.gpa);
     defer engine.deinit();
 
-    var stdin_buffer: [2048]u8 = undefined;
+    var stdin_buffer: [IO_BUF_SIZE]u8 = undefined;
     var stdin_reader: Io.File.Reader = .init(.stdin(), init.io, &stdin_buffer);
 
-    var stdout_buffer: [2048]u8 = undefined;
+    var stdout_buffer: [IO_BUF_SIZE]u8 = undefined;
     var stdout_writer: Io.File.Writer = .init(.stdout(), init.io, &stdout_buffer);
 
-    // TODO only do this when an arg is set -- have this here for collecting log samples for now
-    // var dir = Io.Dir.cwd();
-    // var fname_buf: [128]u8 = undefined;
-    // const start_time = Io.Timestamp.now(init.io, Io.Clock.awake);
-    // const fname = try std.fmt.bufPrint(&fname_buf, "/home/toast/code/zzbot/data/logs/{d}.log", .{start_time.nanoseconds});
-    // var file = try dir.createFile(init.io, fname, .{});
-    // defer file.close(init.io);
-
-    // var keylogger_buffer: [4096]u8 = undefined;
-    // var keylogger_writer = file.writer(init.io, &keylogger_buffer);
+    var command_log: CommandLog = .{};
+    defer command_log.deinit(init.gpa, init.io);
+    if (init.minimal.args.vector.len > 1) {
+        command_log = try CommandLog.init(init.gpa, init.io, init.minimal.args.vector[1]);
+    }
 
     while (try stdin_reader.interface.takeDelimiter('\n')) |line| {
         try engine.process_command(line);
         try engine.flush_actions(&stdout_writer.interface);
-        // try keylogger_writer.interface.print("{s}\n", .{line});
+        try command_log.write(line);
     }
 }
