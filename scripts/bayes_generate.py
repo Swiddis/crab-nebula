0358c5e7f4ad7246093a09c5e5cbe77355a6106d
Simeon Widdis <sawiddis@gmail.com>
2026-05-31 01:39:36 -0700

    run bayes optimization continuously

--- scripts/bayes_generate.py
+++ scripts/bayes_generate.py
@@ -1,5 +1,6 @@
 import hashlib
 import json
+import time
 
 import requests
 from bayes_opt import BayesianOptimization, acquisition
@@ -21,22 +22,25 @@ def get_players():
     return [
         (
             p["rating"],
+            p["rd"],
             {f"x_{i}": weight for i, weight in enumerate(p["model"]["weights"])},
         )
         for p in res
     ]
 
 
-if __name__ == "__main__":
+def do_optimize_loop():
     pbounds = {f"x_{i}": WEIGHT_BOUNDS for i in range(BAYES_PARAM_COUNT)}
     acq = acquisition.UpperConfidenceBound()
     optimizer = BayesianOptimization(f=None, acquisition_function=acq, pbounds=pbounds)
 
     players = get_players()
-    for rating, params in players:
+    rd_ct = 0
+    for rating, rd, params in players:
         optimizer.register(params=params, target=rating)
+        rd_ct += 1 if rd >= 100 else 0
 
-    for i in range(8):
+    for _ in range(16 - rd_ct):
         point = optimizer.suggest()
         model = {"weights": [point[f"x_{i}"] for i in range(BAYES_PARAM_COUNT)]}
         make_player = {
@@ -47,3 +51,9 @@ if __name__ == "__main__":
         print("registering", make_player)
         res = requests.post("http://localhost:8000/player", json=make_player).json()
         assert res["acknowledged"]
+
+
+if __name__ == "__main__":
+    while True:
+        do_optimize_loop()
+        time.sleep(60)
